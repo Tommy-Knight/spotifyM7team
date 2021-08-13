2c2a3da3222b3a7c8260aadbd28baab5b99686f4
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-08-13 16:02:29 +0200

    ooko

--- src/App.js
+++ src/App.js
@@ -1,48 +1,52 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./css/App.css"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import Search from "./components/Search"
-import Albumpage from "./components/Albums"
-import Library from "./components/Library"
-import AlbumDetails from "./components/AlbumDetails"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Search from './components/Search';
+import Albumpage from './components/Albums';
+import Library from './components/Library';
+import AlbumDetails from './components/AlbumDetails';
 
 function App() {
-	return (
-		<div className='App'>
-			<Router>
-				<>
-					<Navbar />
-					<Switch>
-						<Route
-							render={(routerProps) => <Home {...routerProps} title='home' />}
-							path='/home'
-							exact
-						/>
-						<Route
-							render={(routerProps) => <Search {...routerProps} title='Show Details' />}
-							path='/search'
-							exact
-						/>
-						<Route
-							render={(routerProps) => <AlbumDetails {...routerProps} title='' />}
-							path='/album/:albumId'
-						/>
-						{/* <Route exact path="/album/:albumId/" component={AlbumDetails} /> */}
-						<Route
-							render={(routerProps) => <Library {...routerProps} title='' />}
-							path='/Library'
-						/>
+  return (
+    <div className="App">
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route
+              render={(routerProps) => <Home {...routerProps} title="home" />}
+              path="/home"
+              exact
+            />
+            <Route
+              render={(routerProps) => (
+                <Search {...routerProps} title="Show Details" />
+              )}
+              path="/search"
+              exact
+            />
+            <Route
+              render={(routerProps) => (
+                <AlbumDetails {...routerProps} title="" />
+              )}
+              path="/album:albumId"
+            />
+            {/* <Route exact path="/album/:albumId/" component={AlbumDetails} /> */}
+            <Route
+              render={(routerProps) => <Library {...routerProps} title="" />}
+              path="/Library"
+            />
 
-						<Route path='/'>
-							<h1>Welcome to Tommyify</h1>
-						</Route>
-					</Switch>
-				</>
-			</Router>
-		</div>
-	);
+            <Route path="/">
+              <h1>Welcome to Tommyify</h1>
+            </Route>
+          </Switch>
+        </>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,54 +1,51 @@
-import React from "react";
+import React from 'react';
 
 export default function Albums(obj) {
+  console.log('object', obj);
 
-console.log('object', obj )
-	
-	return (
-		<div>
-			<div className='container-fluid m-0 p-0'>
-				<div className='row mt-5'>
-					<div className='col-lg-6 col-md-6 album-n'>
-						<div className='container m-0 p-1'>
-							<img src='./assets/images/album-images/hey-jude.jpg' id='img-album-n' alt='' />
-							<h4>{obj.obj.title}</h4>
-							<p>{obj.obj.artist?.name}</p>
-							<button className='btn btn-success btn-n'>PLAY</button>
-							<p>{obj.obj.nb_tracks} tracks</p>
-							<div>
-								<span className='fa-stack mb-2' id='toggle'>
-									<i className='far fa-heart fa-stack-1x' />
-									<i className='fas fa-heart fa-stack-1x' />
-								</span>
-								<span>
-									<i className='fa fa-ellipsis-h mb-2' />
-								</span>
-							</div>
-						</div>
-					</div>
-					<div className='col-lg-5 col-md-5 tracklist-n'>
-						<div className='track-container'>
-
-						{ obj.obj.tracks.data?.map( track => 
-
-							<ul className='fa-ul'>
-								<li>
-									<span className='fa-li'>
-										<i className='fas fa-music' />
-									</span>
-									{track.title}
-								<span style={{color:"grey", fontSize:"10px"}}>{track.artist.name}</span>								
-								</li>
-							</ul>
-
-						)
-
-						}
-
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="container-fluid m-0 p-0">
+        <div className="row mt-5">
+          <div className="col-lg-6 col-md-6 album-n">
+            <div className="container m-0 p-1">
+              <img
+                src="./assets/images/album-images/hey-jude.jpg"
+                id="img-album-n"
+                alt=""
+              />
+              <h4>{obj.obj.title}</h4>
+              <p>{obj.obj.artist?.name}</p>
+              <button className="btn btn-success btn-n">PLAY</button>
+              <p>{obj.obj.nb_tracks} tracks</p>
+              <div>
+                <span className="fa-stack mb-2" id="toggle">
+                  <i className="far fa-heart fa-stack-1x" />
+                  <i className="fas fa-heart fa-stack-1x" />
+                </span>
+                <span>
+                  <i className="fa fa-ellipsis-h mb-2" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5 col-md-5 tracklist-n">
+            <div className="track-container">
+              {obj.obj.tracks.data.map((track) => (
+                <ul className="fa-ul">
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-music" />
+                    </span>
+                    {/* {track.title} */}
+                    <span />
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
